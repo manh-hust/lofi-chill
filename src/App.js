2825f5cbe4bf738ff711b4d91c96fbb408a0ee9c
ongimkshp <doducmanh.bb@gmail.com>
2022-04-23 11:30:00 +0700

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import {cafe1Img} from './assets/images'
+import {chill1Img} from './assets/images'
+import {chill2Img} from './assets/images'
+
 
 function App() {
   return (
-    <div className="container h-screen px-4 bg-gradient-to-r from-white via-slate-500 to-pink-500">
-      <div className="text-20">
-        Hello world
+    <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-r from-white via-slate-500 to-pink-500">
+      <div className="w-36 h-36 cursor-pointer bg-white shadow-md rounded-lg mx-4 overflow-hidden">
+        <img src={cafe1Img} className="w-full object-cover transition duration-300 ease-in-out hover:scale-105" alt='chill'/>
+      </div>
+      <div className="w-36 h-36 cursor-pointer bg-white shadow-md rounded-lg mx-4 overflow-hidden">
+        <img src={chill1Img} className="w-full object-cover transition duration-300 ease-in-out hover:scale-105" alt='chill'/>
+      </div>
+      <div className="w-36 h-36 cursor-pointer bg-white shadow-md rounded-lg mx-4 overflow-hidden">
+        <img src={chill2Img} className="w-full object-cover transition duration-300 ease-in-out hover:scale-105" alt='chill'/>
       </div>
     </div>
   );
