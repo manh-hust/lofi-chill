7975268634212e9bbae09609d33d1511ad577e8b
manh-hust <doducmanh.bb@gmail.com>
2023-12-15 12:28:58 +0700

    update cidi git

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
